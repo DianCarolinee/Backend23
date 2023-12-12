23f13505e8a4250370d7b9e8f32701ac7bfe156f
IamSebx <brunose0806@gmail.com>
2023-12-12 09:23:54 -0500

    Revert "ChatGPT"
    This reverts commit 00f60910f60909907c3fd173bc8ac45c38a40e07.

--- src/main/java/com/upao/Backend23/config/ChatGPTConfig.java
+++
@@ -1,6 +0,0 @@
-package com.upao.Backend23.config;
-
-public class ChatGPTConfig {
-    public static final String API_KEY = "tu_clave_api";
-    public static final String API_URL = "https://api.openai.com/v1/engines/davinci-codex/completions";
-}
